c62d2f09ae292df0d80fecbdefe1ed372c4abe13
Kat <katherinekelli.creates@gmail.com>
2025-10-15 10:51:51 -0500

    Initial Commit

---
+++ Roll the dice/Roll_the_diceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Roll_the_diceApp.swift
+//  Roll the dice
+//
+//  Created by Katherine Pizzirusso on 10/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Roll_the_diceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
